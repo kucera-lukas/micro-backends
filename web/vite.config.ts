8e8f2ff31eb433b7cb6f3492667d3f205c0a08de
Lukas Kucera <85391931+kucera-lukas@users.noreply.github.com>
2022-09-21 22:19:17 +0200

    Add Google Analytics via vite-plugin-radar (#96)

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,9 @@ export default defineConfig({
       eslint: {
         lintCommand: 'eslint "./src/**/*.{ts,tsx}" --fix',
       },
+    }),
+    VitePluginRadar({
+      analytics: { id: process.env.VITE_GA_MEASUREMENT_ID },
     }),
   ],
 });
